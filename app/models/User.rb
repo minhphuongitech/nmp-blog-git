f20e9e3756e6db20a64752c148a34da316354e79
Nguyen Phuong <nguyen@cuon.co.jp>
2020-10-27 14:54:30 +0900

    Update CRUD code

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,51 +1,14 @@
-require 'bcrypt'
-
-class User < ActiveRecord::Base
-  # users.password_hash in the database is a :string
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
-
-  def create
-    @user = User.new(params[:user])
-    @user.password = params[:password]
-    @user.save!
-  end
-
-  def login
-    @user = User.find_by_email(params[:email])
-    if @user.password == params[:password]
-      give_token
-    else
-      redirect_to home_url
-    end
-  end
-
-  # assign them a random one and mail it to them, asking them to change it
-  def forgot_password
-    @user = User.find_by_email(params[:email])
-    random_password = Array.new(10).map { (65 + rand(58)).chr }.join
-    @user.password = random_password
-    @user.save!
-    Mailer.create_and_deliver_password_change(@user, random_password)
-  end
+class User < ApplicationRecord
+    has_many :articles
+    validates :username, presence: true, 
+                uniqueness: { case_sensitive: false }, 
+                length: {minimum: 3, maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, 
+                uniqueness: { case_sensitive: false }, 
+                length: {maximum: 105}, 
+                format: { with: VALID_EMAIL_REGEX }
+    
+    
+    
 end
-
-my_password = BCrypt::Password.create("my password")
-  #=> "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
-
-my_password.version              #=> "2a"
-my_password.cost                 #=> 10
-my_password == "my password"     #=> true
-my_password == "not my password" #=> false
-
-my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
-my_password == "my password"     #=> true
-my_password == "not my password" #=> false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201021062358) do
+ActiveRecord::Schema.define(version: 20201026102337) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20201021062358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
